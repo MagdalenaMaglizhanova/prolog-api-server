512471d33bc58b562c8b4cde9e8934617aef37b4
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-07-21 23:08:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const os = require("os");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
