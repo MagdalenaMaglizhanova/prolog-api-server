96b2b02d8209338cbfb683ef43127192bcd11835
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-08-24 11:06:53 +0300

    Update manager.pl

--- prolog_files/manager.pl
+++ prolog_files/manager.pl
@@ -1,8 +1,37 @@
-% Зареждаме history
-load_kb('history.pl').
+% ================================================
+% Manager – управлява зареждането на бази знания
+% ================================================
 
-% Използваме я
-sites_by_type(fortress, Name).
+:- dynamic active_kb/1.
 
-% После я махаме
-unload_history.
+% Зарежда нова база знания
+load_kb(File) :-
+    % ако вече има активна база → разтоварваме я
+    active_kb(Current),
+    Current \= File,
+    unload_kb,
+    retractall(active_kb(_)),
+    consult(File),
+    asserta(active_kb(File)),
+    format("Loaded knowledge base: ~w~n", [File]),
+    !.
+
+% Ако няма активна база → директно зареждаме
+load_kb(File) :-
+    \+ active_kb(_),
+    consult(File),
+    asserta(active_kb(File)),
+    format("Loaded knowledge base: ~w~n", [File]).
+
+% Разтоварване на активната база
+unload_kb :-
+    active_kb(File),
+    atom_concat(ModuleName, '.pl', File),
+    unload_file(ModuleName),
+    retractall(active_kb(_)),
+    format("Unloaded knowledge base: ~w~n", [File]),
+    !.
+
+unload_kb :-
+    \+ active_kb(_),
+    format("No active knowledge base to unload.~n").
