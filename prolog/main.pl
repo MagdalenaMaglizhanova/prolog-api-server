f0bb049e55494ef9cd0bf640d67cab1f32f4a9cd
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2026-01-11 14:42:47 +0200

    Update main.pl

--- prolog/main.pl
+++ prolog/main.pl
@@ -1,3 +1,4 @@
+:- encoding(utf8).
 :- dynamic loaded_file/1.
 :- dynamic active_file/1.
 :- dynamic runtime_dir/1.
@@ -81,11 +82,24 @@ consult_file(File) :-
 % Изчиства всички фактове от определен файл
 unload_file(File) :-
     format('Unloading file: ~w~n', [File]),
+    runtime_dir(Dir),
+    atomic_list_concat([Dir, '/', File], Path),
+    
+    % Първо премахваме файла от списъка със заредени
     retractall(loaded_file(File)),
+    
+    % Изчистваме активния файл ако това е той
     (   active_file(File)
     ->  retractall(active_file(File)),
         format('File ~w unloaded and deactivated~n', [File])
     ;   format('File ~w unloaded (was not active)~n', [File])
+    ),
+    
+    % Изчистваме кеша на зареждането
+    (   source_file(Path)
+    ->  abolish_all_tables,
+        unload_file(Path)
+    ;   true
     ).
 
 % Изчиства ВСИЧКИ заредени файлове
@@ -94,6 +108,7 @@ unload_all :-
     (   Files = []
     ->  writeln('No files to unload')
     ;   forall(member(F, Files), unload_file(F)),
+        abolish_all_tables,
         length(Files, Count),
         format('All ~w files unloaded~n', [Count])
     ).
@@ -101,7 +116,21 @@ unload_all :-
 % Презарежда файл (изчиства стари факти и зарежда нови)
 reconsult_file(File) :-
     format('Reconsulting file: ~w~n', [File]),
-    unload_file(File),
+    runtime_dir(Dir),
+    atomic_list_concat([Dir, '/', File], Path),
+    
+    % Изчистване на стария файл
+    (   source_file(Path)
+    ->  unload_file(Path),
+        format('Unloaded previous version of ~w~n', [File])
+    ;   true
+    ),
+    
+    % Изчистване от нашия списък
+    retractall(loaded_file(File)),
+    (   active_file(File) -> retractall(active_file(File)) ; true),
+    
+    % Зареждане на новия файл
     consult_file(File).
 
 % Превключване към нов файл (unload стар + load нов)
@@ -117,20 +146,23 @@ switch_file(NewFile) :-
 % KNOWLEDGE BASE MANAGEMENT
 % ========================================
 
-% Изчиства всички динамични предикати (общ подход)
+% Изчиства всички динамични предикати от активния файл
 clear_all_facts :-
-    writeln('Clearing all dynamic predicates from active file...'),
+    writeln('Clearing all dynamic facts...'),
     (   active_file(File)
     ->  format('Active file: ~w~n', [File]),
-        % Това е генерален подход - ще изчисти ВСИЧКИ динамични предикати
-        current_predicate(Pred/Arity),
-        predicate_property(Pred, dynamic),
-        format('  Retracting: ~w/~w~n', [Pred, Arity]),
-        retractall(Pred),
-        fail  % принуждава backtracking за всички предикати
-    ;   true
-    ),
-    writeln('All dynamic facts cleared').
+        % Намираме всички динамични предикати и ги изчистваме
+        findall(Pred/Arity, (current_predicate(Pred/Arity), predicate_property(Pred, dynamic)), Predicates),
+        (   Predicates = []
+        ->  writeln('No dynamic predicates found')
+        ;   forall(member(Pred/Arity, Predicates), 
+                (format('  Retracting: ~w/~w~n', [Pred, Arity]),
+                 retractall(Pred))
+              ),
+            format('Cleared ~w dynamic predicates~n', [length(Predicates)])
+        )
+    ;   writeln('No active file selected')
+    ).
 
 % ========================================
 % INFORMATION COMMANDS
@@ -152,11 +184,13 @@ current_file :-
 % Показва всички предикати в активния файл
 list_predicates :-
     (   active_file(File)
-    ->  format('Dynamic predicates in ~w:~n', [File]),
-        current_predicate(Pred/Arity),
-        predicate_property(Pred, dynamic),
-        format('  ~w/~w~n', [Pred, Arity]),
-        fail  % backtrack за всички предикати
+    ->  runtime_dir(Dir),
+        atomic_list_concat([Dir, '/', File], Path),
+        format('Dynamic predicates in ~w (~w):~n', [File, Path]),
+        findall(Pred/Arity, (current_predicate(Pred/Arity), predicate_property(Pred, dynamic)), Predicates),
+        (   Predicates = []
+        ->  writeln('  No dynamic predicates')
+        ;   forall(member(Pred/Arity, Predicates), format('  ~w/~w~n', [Pred, Arity]))
+        )
     ;   writeln('No active file to list predicates from')
-    ),
-    writeln('(end of list)').
+    ).
